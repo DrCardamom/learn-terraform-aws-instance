7fc6597ebdac7a1d358f6c47853454a7a5d21c6a
DrCardamom <erika.shimba@400f.jp>
2022-12-16 15:32:29 +0900

    Create EC2 instances using terraform, set remote backend

---
+++ main.tf
@@ -0,0 +1,36 @@
+//https://developer.hashicorp.com/terraform/tutorials/aws-get-started/aws-build
+
+// list necessary tools, prep for resource
+terraform {
+  backend "remote" {
+    organization="shimba-is-learning-tf"
+    workspaces {
+      name = "shimba-new-workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+// configures provider, details of necessary tools. still prep for resource
+provider "aws" {
+  region = "us-west-2"
+}
+
+// actual thing you want to deal with
+// resource "RESOURCE_TYPE" "RESOURCE_NAME"
+// terraform also creates a unique ID by combining these two like so "aws_instance.app_server"
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+   description = "value of the name tag for EC2 instance"
+   type = string
+   default = "NameDerivedFromVariableBlock"
+}
