68992195e7d32ef89d5ca06f56ca2d4e79dc4879
DrCardamom <erika.shimba@400f.jp>
2023-01-17 15:17:21 +0900

    Run import command

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 // list necessary tools, prep for resource
 terraform {
   backend "remote" {
-    organization="shimba-is-learning-tf"
+    organization = "shimba-is-learning-tf"
     workspaces {
       name = "shimba-new-workspace"
     }
@@ -31,6 +31,18 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = var.instance_name
+    Name = var.instance_name_1
+  }
+}
+
+// importの際にまずinstanceMadeInConsole用のconfig追加しないと
+// コマンド失敗するのでこれ追加
+// importした後にtf planの結果見ながら書き換えるので最初は最小限埋めておけばよし
+resource "aws_instance" "instanceMadeInConsole" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "instanceMadeInConsole"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-variable "instance_name" {
-   description = "value of the name tag for EC2 instance"
-   type = string
-   default = "NameDerivedFromVariableBlock"
+variable "instance_name_1" {
+  description = "value of the name tag for EC2 instance"
+  type        = string
+  default     = "NameDerivedFromVariableBlock_1"
+}
+variable "instance_name_2" {
+  description = "another value of the name tag for EC2 instance"
+  type        = string
+  default     = "NameDerivedFromVariableBlock_2"
 }
